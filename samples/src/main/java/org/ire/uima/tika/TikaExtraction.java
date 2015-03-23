d77d8f027d85297a83fc0353b2f28a9c42d13cb3
Kunal Khaware <nandkunal@gmail.com>
2015-03-23 08:04:23 +0530

    Integrated Tika Code

---
+++ samples/src/main/java/org/ire/uima/tika/TikaExtraction.java
@@ -0,0 +1,19 @@
+package org.ire.uima.tika;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+
+public class TikaExtraction {
+	 public static void main(final String[] args) throws IOException, TikaException {
+		//Assume sample.txt is in your current directory
+		File file = new File("data/sample.txt");
+		//Instantiating Tika facade class
+		Tika tika = new Tika();
+		String filecontent = tika.parseToString(file);
+		System.out.println("Extracted Content: " + filecontent);
+		} 
+
+}
