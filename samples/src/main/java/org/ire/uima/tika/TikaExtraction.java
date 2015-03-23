66c30511e86322664fe0307bd03d5add88a17170
Suhas Bhatt <ks.suhas355@gmail.com>
2015-03-23 21:55:05 +0530

    Tika working module

--- samples/src/main/java/org/ire/uima/tika/TikaExtraction.java
+++ samples/src/main/java/org/ire/uima/tika/TikaExtraction.java
@@ -2,18 +2,48 @@ package org.ire.uima.tika;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 
 public class TikaExtraction {
-	 public static void main(final String[] args) throws IOException, TikaException {
-		//Assume sample.txt is in your current directory
-		File file = new File("data/sample.txt");
-		//Instantiating Tika facade class
+
+	private static final String RAWDATA = "./rawdata";
+	private File out;
+	private PrintWriter writer;
+
+	public void extractAndSaveInFile(File file, int filenum) {
 		Tika tika = new Tika();
-		String filecontent = tika.parseToString(file);
-		System.out.println("Extracted Content: " + filecontent);
-		} 
+		try {
+			String filecontent = tika.parseToString(file);
+			out = new File("./data/" + filenum + ".txt");
+			writer = new PrintWriter(out);
+			writer.println(file.getName());
+			writer.println(filecontent);
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TikaException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void extractTextFromRawData() {
+		File folder = new File(RAWDATA);
+		File[] listOfFiles = folder.listFiles();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			extractAndSaveInFile(listOfFiles[i], i);
+		}
+	}
+
+	public static void main(final String[] args) throws IOException,
+			TikaException {
+		TikaExtraction obj = new TikaExtraction();
+		obj.extractTextFromRawData();
+
+	}
 
 }
