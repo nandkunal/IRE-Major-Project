f0cf4a0a8360b74dc4ad2146b4c58e1d955ca126
Suhas Bhatt <ks.suhas355@gmail.com>
2015-03-25 23:56:19 +0530

    score calculator

--- samples/src/main/java/org/ire/util/ScoreCalculator.java
+++ samples/src/main/java/org/ire/util/ScoreCalculator.java
@@ -1,12 +1,134 @@
 package org.ire.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class ScoreCalculator {
 	private TreeMap<String,HashMap<String, Integer> > wordSet;
+	private static final String OUTPUT_PATH="./wordScore/word.txt";
+	private static final String INPUTFOLDER="./token-output";
+	private PrintWriter pr;
+	private List<String> fileNames;
 	
 	public ScoreCalculator() {
 		wordSet = new TreeMap<String, HashMap<String,Integer>>();
+		fileNames = new ArrayList<String>();
+		try {
+			pr=new PrintWriter(new File(OUTPUT_PATH));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void process(){
+		File files = new File(INPUTFOLDER);	
+		String inpFiles[]=files.list();
+		
+		for(int i=0;i<inpFiles.length;i++){
+			System.out.println("calculating TF/IDF of "+inpFiles[i]);
+			extractWordScoreFromFile(inpFiles[i]);
+		}
+	}
+	
+	public void extractWordScoreFromFile(String file){
+		try {
+			Scanner sc = new Scanner(new File(INPUTFOLDER+"/"+file));
+			
+			String actualFileName=null,data=null;
+			if(sc.hasNext()){
+				 actualFileName = sc.nextLine();
+				 fileNames.add(actualFileName);
+			}
+			if(sc.hasNext())
+				data =sc.nextLine();
+			
+			String dataWord[] = data.split(",");
+			HashMap<String, Integer> temp;
+			for(int i=0;i<dataWord.length;i++){
+				//System.out.println(dataWord[i]);
+				if(wordSet.containsKey(dataWord[i])){
+					if(wordSet.get(dataWord[i]).containsKey(actualFileName)){
+						wordSet.get(dataWord[i]).put(actualFileName, wordSet.get(dataWord[i]).get(actualFileName)+1);
+						/*temp=wordSet.get(dataWord[i]);
+						int freq = temp.get(actualFileName);
+						freq++;
+						temp.put(actualFileName, freq);
+						wordSet.put(dataWord[i], temp);
+						if("ac".equals(dataWord[i])){
+							System.out.println("ac-----"+freq);
+						}*/
+					}else{
+						wordSet.get(dataWord[i]).put(actualFileName,1);
+					}
+				}else{
+					HashMap<String, Integer> value = new HashMap<String, Integer>();
+					value.put(actualFileName, 1);
+					wordSet.put(dataWord[i], value);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public void printResult(){
+		System.out.println("-------result----------");
+		System.out.print("FileNames,");
+		
+		for(String file:fileNames){
+			System.out.print(file+",");
+		}
+		System.out.println();
+		for(Map.Entry<String, HashMap<String, Integer>> entry: wordSet.entrySet() ){
+			System.out.print(entry.getKey());
+			System.out.print(",");
+			for(String file:fileNames){
+				if(entry.getValue().containsKey(file)){
+					System.out.print(entry.getValue().get(file));
+				}
+				else
+					System.out.print("0");
+				System.out.print(",");
+			}
+			System.out.println();
+		}
+	}
+	public void printOutPut(){
+		pr.print("FileNames,");
+		for(String file:fileNames){
+			pr.print(file+",");
+		}
+		pr.println();
+		for(Map.Entry<String, HashMap<String, Integer>> entry: wordSet.entrySet() ){
+			pr.print(entry.getKey());
+			pr.print(",");
+			for(String file:fileNames){
+				if(entry.getValue().containsKey(file)){
+					pr.print(entry.getValue().get(file));
+				}
+				else
+					pr.print("0");
+				pr.print(",");
+			}
+			pr.println();
+		}
+	}
+	public void close(){
+		pr.close();
+	}
+	
+	public static void main(String[] args) {
+		ScoreCalculator sc = new ScoreCalculator();
+		sc.process();
+		//sc.printResult();
+		sc.printOutPut();
+		sc.close();
 	}
 }
