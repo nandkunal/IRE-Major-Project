3e32c5694abbea7c9f37f5a1d1c8ff195b877f59
Krishna Gupta <gandhikrishna81@gmail.com>
2015-03-31 21:09:30 +0530

    weka

---
+++ src/main/java/org/ire/uima/tika/TikaExtraction.java
@@ -0,0 +1,51 @@
+package org.ire.uima.tika;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+
+public class TikaExtraction {
+
+	private static final String RAWDATA = "./rawdata";
+	private File out;
+	private PrintWriter writer;
+
+	public void extractAndSaveInFile(File file, int filenum) {
+		Tika tika = new Tika();
+		try {
+			String filecontent = tika.parseToString(file);
+			out = new File("./data/" + filenum + ".txt");
+			writer = new PrintWriter(out);
+			writer.println(file.getName());
+			writer.println(filecontent);
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (TikaException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void extractTextFromRawData() {
+		File folder = new File(RAWDATA);
+		File[] listOfFiles = folder.listFiles();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			System.out.println("extracting data from " + listOfFiles[i] + " using tika");
+			extractAndSaveInFile(listOfFiles[i], i);
+			System.out.println("------------------------------------------");
+		}
+	}
+
+	public static void main(final String[] args) throws IOException,
+			TikaException {
+		TikaExtraction obj = new TikaExtraction();
+		obj.extractTextFromRawData();
+
+	}
+
+}
---
+++ src/main/java/org/ire/util/FileExtractor.java
@@ -0,0 +1,66 @@
+package org.ire.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.attribute.AclEntry.Builder;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class FileExtractor {
+	
+	private static final String SPACE = " ";
+	private static final String STOPWORD = "resources/stopwords.txt";
+	private String file;
+	private Scanner sc;
+	private String document;
+	private Set<String> stopWordSet;
+	private File input;
+	
+	public FileExtractor() {
+		
+		stopWordSet = new HashSet<String>();
+		System.out.println("Loading Stopwords..");
+		try {
+			sc = new Scanner(new File(STOPWORD));
+			String temp;
+			while(sc.hasNext()){
+				temp=sc.nextLine();
+				if(!stopWordSet.contains(temp))
+					stopWordSet.add(temp);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+	
+	public String getDocument(String inputTextFile){
+		StringBuilder builder = null;
+		try {
+			File input = new File("./data/"+inputTextFile);
+			sc = new Scanner(input);
+			String line,words[];
+			builder = new StringBuilder();
+			while(sc.hasNext()){
+				line=sc.nextLine();
+				line=line.trim();
+				words=line.split(SPACE);
+				for(int i=0;i<words.length;i++){
+					words[i]=words[i].toLowerCase();
+					if(!stopWordSet.contains(words[i])){
+						words[i]=words[i].replaceAll("\\W", "");
+						System.out.print(words[i]+SPACE);;
+						builder.append(words[i]);
+						builder.append(SPACE);
+					}
+				}
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return builder.toString();
+	}
+}
